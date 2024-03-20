6d8709fbc8c305f0393991e0f6a76dfff8dd5f63
Алексей Триголос <alexvlg34@gmail.com>
2024-03-20 16:09:17 +0300

    Updated models and changed addition of skills and interests

--- active_interaction.rb
+++ active_interaction.rb
@@ -8,12 +8,12 @@ end
 
 class Interest < ApplicationRecord
   has_many :interests_users, dependent: :destroy
-  has_many :interests, through: :interests_users
+  has_many :users, through: :interests_users
 end
 
 class Skill < ApplicationRecord
   has_many :skills_users, dependent: :destroy
-  has_many :skills, through: :skills_users
+  has_many :users, through: :skills_users
 end
 
 class InterestsUser < ApplicationRecord
@@ -34,17 +34,11 @@ end
 module Users
   class Create < ActiveInteraction::Base
     hash :params do
-      string :name
-      string :surname, default: nil
-      string :patronymic
-      string :fullname, default: nil
-      string :email
+      string :name, :patronymic, :email, :nationality, :country, :gender
       integer :age
-      string :nationality
-      string :country
-      string :gender
+
+      string :surname, :fullname, :skills, default: nil
       array :interests, default: nil
-      string :skills, default: nil
     end
 
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -58,20 +52,19 @@ module Users
     set_callback :validate, :before, -> { self.email = email.downcase }
     set_callback :create, :before, -> { self.fullname ||= [surname, name, patronymic].join(' ').squish }
 
+    def to_model
+      User.new
+    end
+
     def execute
-      define_interests_and_skills
-      user = User.new(params)
+      user = User.new(params.except(:interests, :skills))
+
+      params['interests'].each { |interest| user.interests << Interest.find_or_initialize_by(name: interest) }
+      params['skills'].split(',').each { |skill| user.skills << Skill.find_or_initialize_by(name: skill) }
 
       errors.merge!(user.errors) unless user.save
 
       user
-    end
-
-    private
-
-    def define_interests_and_skills
-      params[:interests] = Interest.where(name: params[:interests]) if params[:interests].present?
-      params[:skills] = Skill.where(name: params[:skills].split(',')) if params[:skills].present?
     end
   end
 end
